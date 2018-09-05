e3088fa48bde4dc5ef869f9ca6e77848d90ce9b4
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-05 08:14:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 
-var showCommits = (el) => {
-  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
-    $('#details').html(renderCommits(data))
-  }).fail(error => {
-    displayError()
-  })
-}
-
-var renderSearchResult = (result) => {
-  return `
-      <div>
-        <h2><a href="${result.html_url}">${result.name}</a></h2>
-        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-        <p>${result.description}</p>
-      </div>
-      <hr>
-    `
-}
-
 var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
 
 var searchRepositories = () => {
