18a5972759325e5646525d39a4b13c0e51f51f50
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-05 08:20:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $.get( "https://api.github.com/users/AhmadAbdulkhaliq", function( data ) {
 console.log("success"+ data)
-
-  
+.fail(function() {
+    console.log('Something went wrong: ');
+});  
 });
