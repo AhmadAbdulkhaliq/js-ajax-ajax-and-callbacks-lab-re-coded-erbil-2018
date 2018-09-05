26af06d7d2ae686fe82d76df7c50b78b2db304ee
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-05 08:15:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 
-var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
-
-var searchRepositories = () => {
-  const searchTerms = $('#searchTerms').val()
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
       $('#results').html(renderSearchResults(data))
     }).fail(error => {
       displayError()
-    })
 }
 
-$(document).ready(function (){
-});
